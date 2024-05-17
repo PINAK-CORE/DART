6e27a35198d496de74368f81553f49f869952425
PINAK TILAVAT <dingo.in4k@gmail.com>
2024-05-17 12:46:17 -0700

    double methods dart file added

---
+++ doubleMethods.dart
@@ -0,0 +1,48 @@
+void main(){
+
+  // Double Methods 
+
+  double doubleValue = 10.35;
+
+  print(doubleValue.hashCode);
+
+  print(doubleValue.isFinite);
+
+  print(doubleValue.isInfinite);
+  
+  print(doubleValue.isNaN);
+
+  print(doubleValue.isNegative);
+
+  print(doubleValue.runtimeType);
+
+  print(doubleValue.sign);
+
+  print(doubleValue.abs());
+
+  print(doubleValue.ceil());
+
+  print(doubleValue.ceilToDouble());
+
+  print(doubleValue.clamp(8, 10));
+
+  print(doubleValue.compareTo(9));
+
+  print(doubleValue.floor());
+
+  print(doubleValue.floorToDouble());
+
+  print(doubleValue.remainder(4));
+
+  print(doubleValue.round());
+
+  print(doubleValue.roundToDouble());
+
+  print(doubleValue.toDouble());
+
+  print(doubleValue.toString());
+
+  print(doubleValue.toInt());
+
+  
+}
