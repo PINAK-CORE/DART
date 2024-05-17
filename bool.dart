320104f485ba21d23f1934c74b8a089331d4eb16
PINAK TILAVAT <dingo.in4k@gmail.com>
2024-05-17 14:29:40 -0700

    bool.dart file added

---
+++ bool.dart
@@ -0,0 +1,13 @@
+void main()
+{
+
+  bool isDrakePDF = true;
+
+  print("Drake PDF Status $isDrakePDF");
+  
+  print("Drake PDF Status ${isDrakePDF.toString()}");
+
+
+
+
+}
--- demo.dart
+++ demo.dart
@@ -1,8 +1,8 @@
 main()
 {
   
-  
+  print("demo file");
 
-  
+
   
 }
