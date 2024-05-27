e469bbf9855d98a79c36f7bf8173312b05600104
PINAK TILAVAT <dingo.in4k@gmail.com>
2024-05-26 22:48:47 -0700

    dart files added

---
+++ ExceptionHandaling/error.dart
@@ -0,0 +1,22 @@
+void main() 
+{
+
+  int a = 10;
+  int b = 0;
+
+  try 
+  {
+    // double c = a / b ;
+    // print(c)  ;
+
+    print(10~/0);
+  } 
+  catch (e) 
+  {
+    print(e);
+  }
+  finally
+  {
+    print("even if we got error this code will still execute");
+  }
+}
